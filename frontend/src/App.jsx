92b87502991358f293d5c77a6f5d52c147fcacb3
Pranav Parulekar <127219508+PranavParulekar-12@users.noreply.github.com>
2025-01-19 11:52:15 +0530

    Commit 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-// import Home from "./Components/Home";
-// import Student from "./Components/Student";
-// import Faculty from "./Components/Faculty";
-// import Course from "./Components/Course";
-// import AddLab from "./Components/AddLab";
+import Home from "./Components/Home";
+import Student from "./Components/Student";
+import Faculty from "./Components/Faculty";
+import Course from "./Components/Course";
+import AddLab from "./Components/AddLab";
 import FHome from "./Components/FacultyModule/FHome";
 import CourseReport from "./Components/FacultyModule/CourseReport";
-// import Course from "./Admin/Course";
+
 
 const App = () => {
   return (
-     <div>
     
-    {/* //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/student" element={<Student />} />
-    //     <Route path="/faculty" element={<Faculty />} />
-    //     <Route path="/course" element={<Course />} />
-    //     <Route path="/addlab" element={<AddLab />} />
-    //   </Routes> */}
-    <Routes>
-      <Route path="/" element={<FHome/>}/>
+     <div>
+    <Navbar/>
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/student" element={<Student />} />
+         <Route path="/faculty" element={<Faculty />} />
+         <Route path="/course" element={<Course />} />
+         <Route path="/addlab" element={<AddLab />} />    
+      <Route path="/FHome" element={<FHome/>}/>
       <Route path="/report" element={<CourseReport/>}/>
-    </Routes>
+    
+       </Routes>
+    
+    
     
 </div>
     
