c0f78220b620a6319f7fd516c282ff3770edbd43
LalitBarage <lalitbarage18092004@gmail.com>
2025-01-26 04:33:21 +0530

    Refactor App.jsx for improved readability and formatting

--- student/src/App.jsx
+++ student/src/App.jsx
@@ -1,7 +1,12 @@
 import { useContext, useEffect } from "react";
 import axios from "axios";
 import { Context } from "./main";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
@@ -9,7 +14,8 @@ import CourseReport from "./Components/CourseReport";
 import LabReport from "./Components/LabReport";
 
 function App() {
-  const { isAuthenticated, setIsAuthenticated, user, setUser } = useContext(Context);
+  const { isAuthenticated, setIsAuthenticated, user, setUser } =
+    useContext(Context);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -33,17 +39,26 @@ function App() {
     <Router>
       {isAuthenticated && <Navbar />}
       <Routes>
-        <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/"
+          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+        />
         <Route
           path="/course/:courseId/report"
-          element={isAuthenticated ? <CourseReport /> : <Navigate to="/login" />}
+          element={
+            isAuthenticated ? <CourseReport /> : <Navigate to="/login" />
+          }
         />
         <Route
           path="/lab/:labid/report"
           element={isAuthenticated ? <LabReport /> : <Navigate to="/login" />}
         />
-       
-       
+        <Route
+          path="/login"
+          element={
+            isAuthenticated ? <Navigate to="/" /> : <Login setUser={setUser} />
+          }
+        />
       </Routes>
     </Router>
   );
