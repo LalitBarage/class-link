fcbf89da8bc88354cf1c64c642b0cb19177c13f6
LalitBarage <lalitbarage18092004@gmail.com>
2025-01-25 17:14:39 +0530

    com

--- faculty/src/Components/Home.jsx
+++ faculty/src/Components/Home.jsx
@@ -95,7 +95,7 @@ const Home = () => {
                     onClick={() => handleCardClick(item.courseId || item.labid)}
                     className="flex items-center gap-2 bg-black text-white px-4 py-2 rounded-md"
                   >
-                    Lecture
+                    Detail
                   </button>
                   {/* Document button */}
                   <button
